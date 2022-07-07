07dbd690fa2f1f17cf287710a461e4ff843376ee
Jon <38672162+gainsley@users.noreply.github.com>
2022-07-07 09:33:37 -0700

    split proto definitions into separate files based on CAMARA working groups (#2)

---
+++ dme/doc.go
@@ -0,0 +1,3 @@
+// Empty .go file so that other repositories can import the protobuf
+// files using golang dependency management (go requires a package to import).
+package distributed_match_engine
